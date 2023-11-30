970f69e033a71d746fb8ee7bfeabab54c5c2c888
samiularif <samiularif0055@gmail.com>
2023-11-30 18:58:37 +0600

    updated

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+//import './App.css';
+
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+function App() {
+  //   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  //   const handleSelectItem = (item: string) => {
+  //     console.log(item);
+  //   };
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const handleButtonClick = () => {
+    setAlertVisibility(true);
+  };
+  const onClose = () => {
+    setAlertVisibility(false);
+  };
+  return (
+    <div>
+      {/* <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      /> */}
+      {alertVisible && (
+        <Alert onClose={onClose}> Your account has been created</Alert>
+      )}
+      <Button
+        children="Signup"
+        color="secondary"
+        onClick={handleButtonClick}
+      ></Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+	const name = 'tanjim';
+	return <h1>hello {name}</h1>;
+}
+
+export default Message;
